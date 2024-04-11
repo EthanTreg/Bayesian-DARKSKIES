Changed gaussian_dataset.py to have different radii. Fixed accuracy calculation in clustering.py. Fixed incorrect latent space fetching in Autoencoder.

src/gaussian_dataset.py
@@ -1,5 +1,5 @@
 """
-Generates Gaussian images with different centers
+Generates Gaussian images with different radii
 """
 import pickle
 
@@ -10,41 +10,50 @@ def main():
     """
     Main function to generate Gaussian images
     """
-    std = 2e-3
-    mu_std = 6e-2
-    centers = np.array([0.05, 0.1, 0.3, 0.4, 0.6, 0.7, 0.75, 1])
-    shape = [len(centers), int(3e3), 1000, 2]
+    num = int(3e3)
+    min_counts = 200
+    count_density = int(5e3)
+    std = 5e-2
+    mu_std = 5e-1
+    data = []
+    ranges = []
     image_shape = [2, 100, 100]
+    radii = np.array([0.05, 0.1, 0.3, 0.4, 0.6, 0.7, 0.75, 1])
 
     # Initialise date
-    mu_offsets = np.random.normal(0, mu_std, size=shape[:2])
-    data = np.random.multivariate_normal(
-        [0, 0],
-        [[std, 0], [0, std]],
-        size=np.prod(shape[:3]),
-    ).reshape(shape)
-    data[..., 0] += centers[:, np.newaxis, np.newaxis] + mu_offsets[..., np.newaxis]
-    images = np.zeros((*shape[:2], *image_shape))
+    centers = np.random.normal(0, mu_std, size=[len(radii), num, 1, 2])
 
-    # Convert data x-y values to an image
-    data_range = np.stack((np.min(data, axis=(0, 1, 2)), np.max(data, axis=(0, 1, 2))))
-    image_data = (
-            ((data - data_range[0]) / (data_range[1] + 1e-6 - data_range[0])) * image_shape[1:]
-    ).astype(int)
-    idxs = np.concatenate((
-        np.arange(image_data.shape[0]).repeat(np.prod(image_data.shape[1:3]))[:, np.newaxis],
-        np.tile(
-            np.arange(image_data.shape[1]).repeat(image_data.shape[-2]),
-            image_data.shape[0],
-        )[:, np.newaxis],
-        image_data.reshape(-1, image_data.shape[-1]),
-    ), axis=-1)
+    # Generate Gaussian distributions for each radius
+    for radius, center in zip(radii, centers):
+        counts = max(min_counts, int(count_density * np.pi * radius ** 2))
+        data.append(np.random.multivariate_normal(
+            [0, 0],
+            [[radius, 0], [0, radius]],
+            size=num * counts,
+        ).reshape([num, counts, 2]))
+        radius_scatter = 1 + np.random.normal(0, std, size=(num, 1, 1))
+        data[-1] = data[-1] * radius_scatter + center
+        ranges.append([np.min(data[-1], axis=(0, 1)), np.max(data[-1], axis=(0, 1))])
 
-    np.add.at(images[:, :, 0], (idxs[:, 0], idxs[:, 1], idxs[:, 2], idxs[:, 3]), 1)
-    np.add.at(images[:, :, 1], (idxs[:, 0], idxs[:, 1], idxs[:, 2], idxs[:, 3]), 1)
+    ranges = np.stack(ranges)
+    ranges = np.stack([np.min(ranges[:, 0], axis=0), np.max(ranges[:, 1], axis=0)])
+    images = np.zeros((len(radii), num, *image_shape))
+
+    # Convert to x-y images for each radii
+    for i, datum in enumerate(data):
+        image_data = (
+                ((datum - ranges[0]) / (ranges[1] + 1e-6 - ranges[0])) * image_shape[1:]
+        ).astype(int)
+        idxs = np.concatenate((
+            np.arange(image_data.shape[0]).repeat(image_data.shape[-2])[:, np.newaxis],
+            image_data.reshape(-1, image_data.shape[-1]),
+        ), axis=-1)
+
+        np.add.at(images[i, :, 0], tuple([*idxs.swapaxes(0, 1)]), 1)
+        np.add.at(images[i, :, 1], tuple([*idxs.swapaxes(0, 1)]), 1)
 
     # Generate labels and normalise images
-    labels = centers.repeat(images.shape[1])
+    labels = radii.repeat(images.shape[1])
     images = images.reshape(-1, *images.shape[2:])
     images /= np.max(images, axis=(-2, -1))[..., np.newaxis, np.newaxis]
 

src/main.py
@@ -51,20 +51,20 @@ def init(config: dict | str = '../config.yaml') -> tuple[
     states_dir = config['output']['network-states-directory']
 
     # Fetch dataset
-    dataset = DarkDataset(
-        data_path,
-        [
-            'CDM+baryons',
-            'SIDM0.1+baryons',
-            'SIDM0.3+baryons',
-            'SIDM1+baryons',
-            'CDM_hi+baryons',
-            'CDM_low+baryons',
-            'zooms'
-            # 'vdSIDM+baryons',
-        ],
-    )
-    # dataset = GaussianDataset('../data/gaussian_data.pkl')
+    # dataset = DarkDataset(
+    #     data_path,
+    #     [
+    #         'CDM+baryons',
+    #         'SIDM0.1+baryons',
+    #         'SIDM0.3+baryons',
+    #         'SIDM1+baryons',
+    #         'CDM_hi+baryons',
+    #         'CDM_low+baryons',
+    #         'zooms'
+    #         # 'vdSIDM+baryons',
+    #     ],
+    # )
+    dataset = GaussianDataset('../data/gaussian_data.pkl')
 
     # Initialise network
     if load_num:
@@ -89,10 +89,10 @@ def init(config: dict | str = '../config.yaml') -> tuple[
         )
 
     # Initialise datasets
-    dataset.normalise(
-        idxs=torch.isin(dataset.labels, torch.unique(dataset.labels)[unknown:]),
-        transform=net.transform,
-    )
+    # dataset.normalise(
+    #     idxs=torch.isin(dataset.labels, torch.unique(dataset.labels)[unknown:]),
+    #     transform=net.transform,
+    # )
     net.classes = torch.unique(dataset.labels).to(device)
     loaders = loader_init(dataset, batch_size=batch_size, val_frac=val_frac, idxs=net.idxs)
     net.idxs = dataset.idxs
@@ -142,7 +142,7 @@ def main(config_path: str = '../config.yaml'):
     )
     data = network.predict(
         loaders[1],
-        path='../data/cluster_val_hydro.csv',
+        path='../data/cluster_val_hydro.pkl',
     )
     plots.plot_clusters(
         f'{plots_dir}PCA',

src/networks/clustering.py
@@ -312,7 +312,7 @@ class ClusterEncoder(BaseNetwork):
         data = super().predict(loader, header=header)
         data['latent'][:, 0] = (data['latent'][:, 0] * self.scale.detach().cpu().numpy() *
                                 self.transform[1] + self.transform[0])
-        accuracy = np.count_nonzero(data['targets'] == data['preds']) / len(data['ids'])
+        accuracy = np.count_nonzero(data['targets'].flatten() == data['preds']) / len(data['ids'])
         print(f"Accuracy: {accuracy:.1%}")
         self._save_predictions(path, data)
         return data

src/networks/encoder_decoder.py
@@ -73,7 +73,7 @@ class Autoencoder(BaseNetwork):
         """
         bounds = torch.tensor([0., 1.]).to(self._device)
         output = self.net(in_data)
-        latent = self.net.clone
+        latent = self.net.checkpoints[-1]
 
         loss = nn.MSELoss()(output, in_data) + self.net.kl_loss
 

src/utils/data.py
@@ -274,9 +274,9 @@ class GaussianDataset(Dataset):
 
         # self.labels[np.in1d(self.labels, 0.12)] = 1e-6
         # self.labels[np.in1d(self.labels, 0.4)] = 1e-5
-        self.labels[np.in1d(self.labels, 0.75)] = 1e-4
+        self.labels[np.in1d(self.labels, 0.4)] = 1e-4
 
-        bad_idxs = np.in1d(self.labels, [0.7, 0.4])
+        bad_idxs = np.in1d(self.labels, [0.75, 0.7])
         self.labels = np.delete(self.labels, bad_idxs, axis=0)
         self.images = np.delete(self.images, bad_idxs, axis=0)
 